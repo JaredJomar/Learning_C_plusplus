21f3d517538de1ecc7d87e231826f943a8b61866
JaredJomar <98923499+JaredJomar@users.noreply.github.com>
2022-02-24 21:47:07 -0400

    Add files via upload

--- Learning_C++/tutorial_12.cpp
+++ Learning_C++/tutorial_12.cpp
@@ -4,17 +4,21 @@ using namespace std;
 
 int main()
 {
-    int x = 1;
+    int x = 4; // Cambia el valor de x para ver los diferentes casos
     switch (x + 1)
     {
     case 1:
         cout << "cool \n";
         break;
-    case 2:                     // x = 1 and 1 + 1 = 2 por lo que es switch da el resultado del case 2
+    case 2: // x = 1 and 1 + 1 = 2 por lo que es switch da el resultado del case 2
         cout << "not cool \n";
+        break;
+    case 10:
+        cout << "interesting \n";
         break;
 
     default:
+        cout << "default"; // Si no se encuentra ningún caso se muestra el default 
         break;
     }
 }
